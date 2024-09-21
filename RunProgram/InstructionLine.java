b50b13fd4224bfbf95dbb085b0fdc70076bd6ec1
Your Name <you@example.com>
2024-09-21 08:40:12 +0700

    espescially , delete print line of  j-type

--- RunProgram/InstructionLine.java
+++ RunProgram/InstructionLine.java
@@ -71,8 +71,7 @@ public class InstructionLine
                     break;
                 case "beq": I_type.gen_machine_code(parts);
                     break;
-                case "jalr": J_type.gen_machine_code(parts);
-                            System.out.println("JALR JAh"); 
+                case "jalr": J_type.gen_machine_code(parts); 
                     break;
                 case "halt": O_type.gen_machine_code(parts);
                     break;
